5806544806ab040591c754c4a444d50ad3283bd7
Philip Hendry <phil@philiphendry.me.uk>
2022-08-14 12:40:28 +0100

    Add Schlick's reflectance approximation

--- Materials/DielectricMaterial.cs
+++ Materials/DielectricMaterial.cs
@@ -20,10 +20,22 @@ public class DielectricMaterial : MaterialBase
         var sinTheta = (float)Math.Sqrt(1.0f - cosTheta * cosTheta);
 
         var cannotRefract = refractionIndexRatio * sinTheta > 1.0f;
-        var refractedDirection = cannotRefract
+        var refractedDirection = cannotRefract || Reflectance(cosTheta) > Utility.Random()
                 ? Vector3.Reflect(unitDirection, hitPoint.Normal)
                 : Vector3Utility.Refract(unitDirection, hitPoint.Normal, refractionIndexRatio);
 
         return (attenuation: Albedo, scatteredRay: new Ray(hitPoint.Point, refractedDirection));
+    }
+
+    /// <summary>
+    /// Schlick's appromixation for reflectance
+    /// </summary>
+    /// <param name="cosine"></param>
+    /// <returns></returns>
+    private float Reflectance(float cosine)
+    {
+        var r0 = (1 - _indexOfRefraction) / (1 + _indexOfRefraction);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * (float)Math.Pow(1 - cosine, 5.0f);
     }
 }
--- README.md
+++ README.md
@@ -15,3 +15,5 @@
 ![7 Render With Metal Fuzziness](renderings/7-render-with-metal-fuzziness.png)
 
 ![8 Render With Refraction](renderings/8-render-with-refraction.png)
+
+![9 Render With Schlicks Reflectance](renderings/9-render-with-schlicks-reflectance.png)
